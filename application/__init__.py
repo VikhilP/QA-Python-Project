33f4a0916be4a684229734ceb9677bc7a2a283ea
VikhilP <vikhilp98@gmail.com>
2021-05-23 22:41:19 +0100

    Environment Key

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,9 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(DBConnect)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DBConnect')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-app.config['SECRET_KEY'] = os.environ.get(SecretK)
+app.config['SECRET_KEY'] = os.environ.get('SecretK')
 
 db = SQLAlchemy(app)
 
